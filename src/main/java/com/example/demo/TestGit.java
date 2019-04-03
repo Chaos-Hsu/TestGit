b12707f93d318f4b52310eeb5e14dbe50a5d947c
xqc <18550230517@163.com>
2019-04-03 13:41:58 +0800

    测试提交GIT

---
+++ src/main/java/com/example/demo/TestGit.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * @author: xqc
+ * @date: 2019/4/3
+ * @description:测试提交GIT
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("提交GIT");
+    }
+
+}
